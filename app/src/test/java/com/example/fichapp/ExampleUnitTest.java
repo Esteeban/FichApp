5018770e057e16e7cce2e589f432818efd1e98d7
estebanrojasr <esteban.rojas@forcast.cl>
2022-07-19 21:14:36 -0400

    archivo

--- app/src/test/java/com/example/fichapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/fichapp/ExampleUnitTest.java
@@ -1,8 +1,10 @@
 package com.example.fichapp;
 
-import org.junit.Test;
+
 
 import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
